df40707fa3a7241ee94dabafef5af258146e98f1
Soumik Pradhan <soumik@antarx.tech>
2022-01-14 02:50:49 +0530

    setup test adapter

---
+++ test/adapter.js
@@ -0,0 +1,22 @@
+const MockPromise = require("..");
+
+// In order to test your promise library, a very minimal adapter interface
+// is to be exposed to promises-test
+const adapter = {
+	deferred() {
+		let reject, resolve;
+		const promise = new MockPromise((res, rej) => {
+			reject = rej;
+			resolve = res;
+		});
+		return {
+			promise,
+			resolve,
+			reject,
+		};
+	},
+	resolved: (val) => new MockPromise((res) => res(val)),
+	rejected: (val) => new MockPromise((_, rej) => rej(val)),
+};
+
+module.exports = adapter;
