52903f715637af4246b91c59895ac7bb7345cc8b
Soumik Pradhan <soumik@antarx.tech>
2022-01-15 07:50:37 +0530

    Add comments

--- mockPromiseFunction.js
+++ mockPromiseFunction.js
@@ -1,7 +1,7 @@
 const STATUS = {
-	pending: 0,
-	fulfilled: 1,
-	rejected: 2,
+	pending   : 0,
+	fulfilled : 1,
+	rejected  : 2,
 };
 Object.freeze = STATUS;
 
@@ -18,21 +18,28 @@ function MockPromsie(fn) {
 	let value;
 	let handlers = [];
 
+	// pending -> fulfilled: state transition fn
 	const fulfill = (result) => {
 		status = STATUS.fulfilled;
 		value = result;
+		// call onFulfilled for all attached handlers
 		handlers.forEach(h => h.onFulfill(value));
 	};
 
+	// pending -> rejected: state transition fn
 	const reject = (error) => {
 		status = STATUS.rejected;
 		value = error;
+		// call onRejected for all attached handlers
 		handlers.forEach(h => h.onReject(value));
 	};
 
+	// Call the param fn as soon as MockPromise constructor is called
 	const process = (fn) => {
 		fn(
 			result => {
+				// if result is a MockPromise itself
+				// we are checking if result has .then function
 				const then = getThen(result);
 				if (then) {
 					process(then);
@@ -44,12 +51,13 @@ function MockPromsie(fn) {
 		);
 	};
 
-	const handle = (onFulfill, onReject) => {
+	function handle(onFulfill, onReject) {
 		if(status === STATUS.pending) handlers.push({ onFulfill, onReject });
 		if(status === STATUS.fulfilled) onFulfill(value);
 		if(status === STATUS.rejected) onReject(value);
-	};
+	}
 
+	// onFulfill and onReject are optional parameters in `then` 
 	this.then = (onFulfill, onReject) => {
 		return new MockPromsie((resolve/*, reject */) => {
 			handle(
@@ -73,7 +81,9 @@ function MockPromsie(fn) {
 	process(fn);
 }
 
+// Promise.resolve(2)
 MockPromsie.resolved = (value) => new MockPromsie((res) => res(value));
+// Promise.reject(new Error('dummy'))
 MockPromsie.rejected = (value) => new MockPromsie((_, rej) => rej(value));
 
 module.exports = MockPromsie;
